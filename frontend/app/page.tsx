93d35eaa012c089cca14fefc30457591e3be8fd8
Shakil <121shakilahmed@gmail.com>
2025-12-02 20:58:10 +0600

    frontend added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,24 +6,17 @@ import CTASection from '@/components/home/CTASection'
 import SeeItAction from '@/components/home/SeeItAction'
 import TestimonialsSection from '@/components/home/TestimonialsSection'
 import Footer from '@/components/home/Footer'
-<<<<<<< HEAD
 import AIModelsSection from '@/components/home/AIModelsSection'
 import WorkflowSection from '@/components/home/WorkflowSection'
-=======
->>>>>>> origin/shakil3
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-black">
       <Navbar />
       <HeroSection />
-<<<<<<< HEAD
       <AIModelsSection />
       <FeaturesSection />
       <WorkflowSection />
-=======
-      <FeaturesSection />
->>>>>>> origin/shakil3
       <HowItWorksSection />
       <SeeItAction />
       <TestimonialsSection />
