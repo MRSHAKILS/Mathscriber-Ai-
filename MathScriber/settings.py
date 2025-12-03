7091f07a8027caefca73a48667fe68a18ffe7e4b
Shakil <121shakilahmed@gmail.com>
2025-12-03 06:44:42 +0600

    build req

--- MathScriber/settings.py
+++ MathScriber/settings.py
@@ -9,12 +9,12 @@ load_dotenv(BASE_DIR / '.env')
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*_k*l$0h8ve_0y^u_5ga0t$22ejg-t9@syli!yg9tk0uhyb9tg"
+SECRET_KEY = os.getenv('SECRET_KEY', "django-insecure-*_k*l$0h8ve_0y^u_5ga0t$22ejg-t9@syli!yg9tk0uhyb9tg")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -98,7 +99,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"] 
-STATIC_ROOT = BASE_DIR / "staticfiles"    
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"    
 
 # Media files (User uploaded files like images/outputs)
 MEDIA_URL = "/media/"
